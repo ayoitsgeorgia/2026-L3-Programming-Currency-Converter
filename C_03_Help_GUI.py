d8498e747041bdbf88e3190505955d91dd0c8850
ayoitsgeorgia <georgiapemberton@mmc.school.nz>
2026-03-16 14:12:14 +1300

    Add files via upload

---
+++ C_03_Help_GUI.py
@@ -0,0 +1,91 @@
+from tkinter import *
+from functools import partial  # To prevent unwanted windows
+
+
+class Converter:
+    """
+    Temperature conversion tool (C to F or F to C)
+    """
+
+    def __init__(self):
+        """
+        Temperature converter GUI
+        """
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.to_help_button = Button(self.temp_frame,
+                                     text="Help / Info",
+                                     bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     font=("Arial", "14", "bold"), width=12,
+                                     command=self.to_help)
+        self.to_help_button.grid(row=1, padx=5, pady=5)
+
+    def to_help(self):
+        """
+        Opens help dialogue box and disables help button (so that users can't create multiple help boxes)
+        """
+        DisplayHelp(self)
+
+
+class DisplayHelp:
+
+    def __init__(self, partner):
+        # setup dialogue box and background colour
+        background = "#ffe6cc"
+        self.help_box = Toplevel()
+
+        # disable help button
+        partner.to_help_button.config(state=DISABLED)
+
+        # IF users press the cross at the top, closes help and 'releases' help button
+        self.help_box.protocol('WM_DELETE_WINDOW',
+                               partial(self.close_help, partner))
+
+        self.help_frame = Frame(self.help_box, width=300,
+                                height=200)
+        self.help_frame.grid()
+
+        self.help_heading_label = Label(self.help_frame,
+                                        text="Help / Info",
+                                        font=("Arial", "14", "bold"))
+        self.help_heading_label.grid(row=0)
+
+        help_text = "help text goes here"
+
+        self.help_text_label = Label(self.help_frame,
+                                     text=help_text, wraplength=350,
+                                     justify="left")
+        self.help_text_label.grid(row=1, padx=10)
+
+        self.dismiss_button = Button(self.help_frame,
+                                     font=("Arial", "12", "bold"),
+                                     text="Dismiss", bg="#CC6600",
+                                     fg="#FFFFFF",
+                                     command=partial(self.close_help, partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+        # list and loop to set background colour on everything expect the buttons
+        recolour_list = [self.help_frame, self.help_heading_label,
+                         self.help_text_label]
+
+        for item in recolour_list:
+            item.config(bg=background)
+
+    def close_help(self, partner):
+        """
+        Closes help dialogue box (and enables help button)
+        """
+        # Put help button back to normal
+        partner.to_help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
+
+# main routine
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
